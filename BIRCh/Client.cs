2f9b567181bf7d28d968e0398b54d226d8607d2e
void <git.email.address@googlemail.com>
2013-06-13 06:22:56 +0200

    Added more event handlers

--- BIRCh/Client.cs
+++ BIRCh/Client.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BIRCh
 {
@@ -11,8 +13,19 @@ namespace BIRCh
 		const int BufferSize = 0x1000;
 		const string EndOfLine = "\r\n";
 
+		delegate void MatchHandler(Match match);
+
+		protected string User;
+		protected string Localhost;
+		protected string Host;
+		protected string RealName;
+
+		protected string ActualNick;
+
 		IPEndPoint ServerEndpoint;
 		TcpClient IRCClient;
+
+		Dictionary<string, MatchHandler> MatchHandlers;
 
 		public void Run()
 		{
@@ -23,9 +36,82 @@ namespace BIRCh
 			}
 		}
 
-		protected Client(IPEndPoint serverEndpoint)
+		protected Client(IPEndPoint serverEndpoint, string user, string localhost, string host, string realName)
 		{
+			User = user;
+			Localhost = localhost;
+			Host = host;
+			RealName = realName;
 			ServerEndpoint = serverEndpoint;
+			SetMatchHandlers();
+		}
+
+		#region Default event handlers
+
+		protected virtual void OnEntry()
+		{
+		}
+
+		protected virtual void OnNotice(User user, string target, string message)
+		{
+		}
+
+		protected virtual void OnInvite(User user, string channel)
+		{
+		}
+
+		protected virtual void OnJoin(User user, string channel)
+		{
+		}
+
+		protected virtual void OnMessage(User user, string target, string message)
+		{
+		}
+
+		protected virtual void OnMode(User user, string target, string modes)
+		{
+		}
+
+		protected virtual void OnQuit(User user, string message)
+		{
+		}
+
+		#endregion
+
+		protected void SendMessage(string target, string message)
+		{
+			Send(string.Format("PRIVMSG {0} :{1}", target, message));
+		}
+
+		protected void JoinChannel(string channel)
+		{
+			Send(string.Format("JOIN {0}", channel));
+		}
+
+		protected void ChangeNick(string nick)
+		{
+			Send(string.Format("NICK {0}", nick));
+		}
+
+		protected void SetUser(string user, string localhost, string host, string realName)
+		{
+			Send(string.Format("USER {0} \"{1}\" \"{2}\" \"{3}\"", user, localhost, host, realName));
+		}
+
+		void SetMatchHandlers()
+		{
+			MatchHandlers = new Dictionary<string, MatchHandler>();
+			MatchHandlers["^PING (.+?)$"] = OnPingMatch;
+			MatchHandlers["^([^ ]+?) 376 ([^ ]+?) :?(.+?)$"] = OnEndOfMotdMatch;
+			MatchHandlers["^([^ ]+?) 422 ([^ ]+?) :?(.+?)$"] = OnEndOfMotdMatch;
+			MatchHandlers["^([^ ]+?) 433 (.+?)$"] = OnNickInUseMatch;
+			MatchHandlers["^([^ ]+?) 437 (.+?)$"] = OnNickInUseMatch;
+			MatchHandlers["^([^ ]+?)!([^ ]+?)@([^ ]+?) NOTICE ([^ ]+?) :?(.+?)$"] = OnNoticeMatch;
+			MatchHandlers["^([^ ]+?)!([^ ]+?)@([^ ]+?) INVITE ([^ ]+?) :?(.+?)$"] = OnInviteMatch;
+			MatchHandlers["^([^ ]+?)!([^ ]+?)@([^ ]+?) JOIN ([^ ]+?)$"] = OnJoinMatch;
+			MatchHandlers["^([^ ]+?)!([^ ]+?)@([^ ]+?) PRIVMSG ([^ ]+?) :?(.+?)$"] = OnMessageMatch;
+			MatchHandlers["^([^ ]+?)!([^ ]+?)@([^ ]+?) MODE ([^ ]+?) :?(.+?)$"] = OnModeMatch;
+			MatchHandlers["^([^ ]+?)!([^ ]+?)@([^ ]+?) QUIT :?(.+?)$"] = OnQuitMatch;
 		}
 
 		void Connect()
@@ -63,43 +149,87 @@ namespace BIRCh
 
 		void ParseLine(string line)
 		{
-			string[] tokens = line.Split(' ');
-			if (tokens.Length < 2)
-				throw new Exception("Malformed line");
-			string command = tokens[0];
-			switch (command)
+			foreach (var pair in MatchHandlers)
 			{
-				case "PING":
-					string reply = "PONG " + line.Substring(5);
-					Send(reply);
+				Match match = Regex.Match(line, pair.Key);
+				if (match != null)
+				{
+					pair.Value(match);
 					break;
-
-				case "376":
-				case "422":
-					OnEndOfMotd();
-					break;
-
-				case "433":
-				case "437":
-					OnNickInUse();
-					break;
-
-				case "NOTICE":
-					OnNotice();
-					break;
+				}
 			}
 		}
 
-		void OnEndOfMotd()
+		User GetUser(Match match)
 		{
+			return new User(match.Groups[1].Value, match.Groups[2].Value, match.Groups[3].Value);
 		}
 
-		void OnNickInUse()
+		#region Match handlers
+
+		void OnPingMatch(Match match)
 		{
+			string reply = "PONG " + match.Groups[1].Value;
+			Send(reply);
 		}
 
-		void OnNotice()
+		void OnEndOfMotdMatch(Match match)
 		{
+			ActualNick = match.Groups[2].Value;
+			OnEntry();
 		}
+
+		void OnNickInUseMatch(Match match)
+		{
+			throw new Exception("Nick in use");
+		}
+
+		void OnNoticeMatch(Match match)
+		{
+			User user = GetUser(match);
+			string target = match.Groups[4].Value;
+			string message = match.Groups[5].Value;
+			OnNotice(user, target, message);
+		}
+
+		void OnInviteMatch(Match match)
+		{
+			User user = GetUser(match);
+			string target = match.Groups[4].Value;
+			string channel = match.Groups[5].Value;
+			OnInvite(user, channel);
+		}
+
+		void OnJoinMatch(Match match)
+		{
+			User user = GetUser(match);
+			string channel = match.Groups[4].Value;
+			OnJoin(user, channel);
+		}
+
+		void OnMessageMatch(Match match)
+		{
+			User user = GetUser(match);
+			string target = match.Groups[4].Value;
+			string message = match.Groups[5].Value;
+			OnMessage(user, target, message);
+		}
+
+		void OnModeMatch(Match match)
+		{
+			User user = GetUser(match);
+			string target = match.Groups[4].Value;
+			string modes = match.Groups[5].Value;
+			OnMode(user, target, modes);
+		}
+
+		void OnQuitMatch(Match match)
+		{
+			User user = GetUser(match);
+			string message = match.Groups[4].Value;
+			OnQuit(user, message);
+		}
+
+		#endregion
 	}
 }
---
+++ BIRCh/User.cs
@@ -0,0 +1,16 @@
+﻿namespace BIRCh
+{
+	public class User
+	{
+		public string Nick;
+		public string Ident;
+		public string Host;
+
+		public User(string nick, string ident, string host)
+		{
+			Nick = nick;
+			Ident = ident;
+			Host = host;
+		}
+	}
+}
